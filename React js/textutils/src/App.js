ee90f5549c88b7898ddb83c637cc39416ca0536d
Kavya Krishna chinchankar <kavyachinchankar@gmail.com>
2024-02-28 14:07:15 +0530

    Add files via upload

---
+++ React js/textutils/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+let name='Kavya';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <h3>Hello {name}</h3>
+    <div className="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, modi incidunt! Earum exercitationem corrupti odio fuga alias laboriosam ducimus vel facilis, deleniti qui et labore, aperiam, voluptas consequatur voluptatibus placeat?
+    </div>
+    <img src="" alt="" />
+    <div className="blank">
+      Lovely
+    </div>
+    </>
+  );
+}
+
+export default App;
